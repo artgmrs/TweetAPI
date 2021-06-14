8e6b81544e4853327d55405597f80e4b3b3ce3f8
Arthur Guimarães <arthurdonds@gmail.com>
2021-06-14 18:55:35 -0300

    Commit to create dev branch

---
+++ TweetAPI.Test/Infra/Clients/TwitterClientTests.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using NUnit.Framework;
+using TweetAPI.Core.Repos;
+
+namespace TweetAPI.Test.Infra.Clients
+{
+    public class TwitterClientTests
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var _client = new Mock<ITwitterRepo>();
+
+        }
+    }
+}
