bb6c29d6cdf809a590df93a4ba69f4eaef555755
Arthur Guimarães <arthurdonds@gmail.com>
2021-06-17 20:09:47 -0300

    improved look and feel of the console app

--- TweetAPI/Infra/UI/ConsoleApp.cs
+++ TweetAPI/Infra/UI/ConsoleApp.cs
@@ -1,6 +1,6 @@
 ﻿using Autofac;
+using ShellProgressBar;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using TweetAPI.Core.Repos;
 using TweetAPI.Infra.Autofac;
@@ -12,16 +12,25 @@ namespace TweetAPI.Infra.UI
     {
         static async Task Main()
         {
-            Console.WriteLine("type your query: ");
+            Console.WriteLine("Type your query: ");
             var query = Console.ReadLine();
+            Console.WriteLine();
 
             if (string.IsNullOrEmpty(query))
                 throw new Exception("Invalid query");
 
             var container = AutofacContainerBuilder.Build();
 
+            var options = new ProgressBarOptions()
+            {
+                ProgressCharacter = '─',
+                ProgressBarOnBottom = true
+            };
+
+            using (var progressBar = new ProgressBar(10, "Starting request...", options))
             using (var scope = container.BeginLifetimeScope())
             {
+                var progress = progressBar.AsProgress<int>();
                 var twitterClient = scope.Resolve<ITwitterRepo>();
                 var response = await twitterClient.SearchTweets(query);
 
@@ -32,9 +41,19 @@ namespace TweetAPI.Infra.UI
                 {
                     var content = fileHandler.FormatContent(tweet);
                     await writer.WriteAsync(content);
-                    await writer.FlushAsync();                    
+                    await writer.FlushAsync();
+                    progress.Report(2);
                 }
-                fileHandler.MoveFileToDesktop();
+
+                var deskPath = fileHandler.MoveFileToDesktop();
+                progressBar.Message = "Finished with success!";
+                progressBar.Dispose();
+
+                Console.WriteLine($"The file has been generated at {deskPath}");
+                Console.WriteLine();
+
+                Console.WriteLine("Press any key to exit the program :)");
+                Console.ReadKey();
             }
         }
     }
